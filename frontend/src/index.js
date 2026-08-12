86a17a568c65d17149232a608a0e7a9c31441698
Bryan Anthony Spruk <bryanspruk7@outlook.com>
2026-08-12 00:20:46 -0500

    mount contact review beside PRIME MAIL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import PrimeMailV4 from './PrimeMailV4';
+import ContactReviewOverlay from './ContactReviewOverlay';
+import './ContactReviewOverlay.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<PrimeMailV4 />);
+root.render(
+  <>
+    <PrimeMailV4 />
+    <ContactReviewOverlay />
+  </>
+);
