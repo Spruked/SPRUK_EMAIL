16a7c9ab5ac619eb074d82aa4bf52d4b4c7abc99
Bryan Anthony Spruk <bryanspruk7@outlook.com>
2026-08-26 01:51:54 -0500

    Load VIV Communications shell containment fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import VIVCommunications from './PrimeMailV4';
 import ContactReviewOverlay from './ContactReviewOverlay';
 import './ContactReviewOverlay.css';
+import './VIVCommunicationsFixes.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
