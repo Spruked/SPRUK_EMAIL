d8b27acff975457033c6602feb600c272dfc2dd0
Bryan Anthony Spruk <bryanspruk7@outlook.com>
2026-08-25 22:18:59 -0500

    Make VIV Communications the frontend entry identity

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PrimeMailV4 from './PrimeMailV4';
+import VIVCommunications from './PrimeMailV4';
 import ContactReviewOverlay from './ContactReviewOverlay';
 import './ContactReviewOverlay.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <PrimeMailV4 />
+    <VIVCommunications />
     <ContactReviewOverlay />
   </>
 );
